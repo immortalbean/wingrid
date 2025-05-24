b1561d773c9062a6603778de08c1d594e26596f7
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-05-24 18:41:43 -0400

    changed setup

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="wingrid",
     version="0.1",
+    description="A grid-based UI/windowing system for Pygame",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[],
+    python_requires=">=3.7",
 )

ba8d33bf23518dc6a11b2a9d8e094f3f62b223b5
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-05-25 16:40:16 -0400

    added correct docs link

--- wingrid/__init__.py
+++ wingrid/__init__.py
@@ -4,4 +4,4 @@ from .assets.scripts.core.constants import *
 import os
 import sys
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
-print('WinGrid v0.1 - Python 3.13.1 \nDocs: https://github.com/immortalbean/wingrid/blob/main/readme.md')
+print('WinGrid v0.1 - Python 3.13.1 \nDocs: https://github.com/immortalbean/wingrid/tree/main/docs')

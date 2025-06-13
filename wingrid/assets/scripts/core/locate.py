cbad7256fae06f7b2f64ac863826b87aaf70eb30
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-06-12 20:57:08 -0400

    made locate code cleaner

--- wingrid/assets/scripts/core/locate.py
+++ wingrid/assets/scripts/core/locate.py
@@ -3,4 +3,6 @@ import os
 BASEDIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def asset_path(*paths):
-    return os.path.join(BASEDIR, *paths)
+    path = os.path.join(BASEDIR, *paths)
+    return path
+

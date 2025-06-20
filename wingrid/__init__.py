48f346d4a5bf0b0dcf717954545e9b0f32d96138
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-06-20 02:49:40 -0400

    set version to 0.2

--- wingrid/__init__.py
+++ wingrid/__init__.py
@@ -5,5 +5,5 @@ from .assets.scripts.core.elements import *
 import os
 import sys
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
-ver = 0.1
+ver = 0.2
 print(f"Welcome to WinGrid - v{ver} - (Pygame {pygame.ver}) \nDocs: https://github.com/immortalbean/wingrid/tree/main/docs")

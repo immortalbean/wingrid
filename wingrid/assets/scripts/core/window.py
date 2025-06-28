5a47672b820311e4419f26756ec798525d390cce
ImmortalBean <67293861+immortalbean@users.noreply.github.com>
2025-06-28 15:04:19 -0400

    added get element

--- wingrid/assets/scripts/core/window.py
+++ wingrid/assets/scripts/core/window.py
@@ -79,6 +79,13 @@ class _Window:
             else:
                 if element:
                     self.elements[element.name] = element
+    def get_element(self, element: str):
+        if element in self.elements:
+            return self.elements[element]
+        else:
+            caller = inspect.stack()[1]
+            print(f"[WinGrid] Warning: Element {element} does not exist. Check your code for typos or inconsistencies. (line {caller.lineno} in {caller.filename})",file=sys.stderr)
+            return None
     def tick(self, scale: int, behind_window: bool, surface: pygame.Surface):
         mouse_position = pygame.mouse.get_pos()
         mouse_in_window = False
